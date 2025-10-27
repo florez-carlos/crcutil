b0c759ac95968f506b81e05f48d104eec8b0ec38
Carlos Florez <carlos@florez.co.uk>
2025-10-27 23:52:18 +0000

    fix(Windows): Remove dependency imports incompatible with Windows

--- src/crcutil/core/keyboard_monitor_factory.py
+++ src/crcutil/core/keyboard_monitor_factory.py
@@ -2,9 +2,6 @@ import os
 import platform
 
 from crcutil.core.keyboard_monitor import KeyboardMonitor
-from crcutil.core.keyboard_monitor_wayland import KeyboardMonitorWayland
-from crcutil.core.keyboard_monitor_windows import KeyboardMonitorWindows
-from crcutil.core.keyboard_monitor_x11 import KeyboardMonitorX11
 from crcutil.exception.device_error import DeviceError
 from crcutil.util.static import Static
 
@@ -15,9 +12,20 @@ class KeyboardMonitorFactory(Static):
         system = platform.system()
 
         if system == "Windows":
+            from crcutil.core.keyboard_monitor_windows import (  # noqa:PLC0415
+                KeyboardMonitorWindows,
+            )
+
             return KeyboardMonitorWindows()
         elif system == "Linux":
             session = os.getenv("XDG_SESSION_TYPE") or ""
+
+            from crcutil.core.keyboard_monitor_wayland import (  # noqa:PLC0415
+                KeyboardMonitorWayland,
+            )
+            from crcutil.core.keyboard_monitor_x11 import (  # noqa:PLC0415
+                KeyboardMonitorX11,
+            )
 
             if session.startswith("wayland"):
                 return KeyboardMonitorWayland()
